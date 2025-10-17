6b3901bb171088e05a1a42f18672e860b6e1dc74
ukp <ukpzoom4@gmail.com>
2025-10-17 10:07:51 +0530

    initial

---
+++ index.js
@@ -0,0 +1,11 @@
+const express=require('express')
+
+
+const app=express();
+
+app.get('/',(_,res)=>{
+    res.json('server is up & running...');
+})
+
+
+app.listen(8000,()=>console.log('server is running on port 8000'));
